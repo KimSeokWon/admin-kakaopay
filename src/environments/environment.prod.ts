bc7484f70e1fc08e797a08e4b127bce7c78d50e5
Kim,SeokWon <ukaka.kim@gmail.com>
2022-06-04 08:44:29 +0900

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAd7gm_M2TXbTpOtfQ_4rqBZLiBm2LyC6c',
+    authDomain: 'sam-app-c17ad.firebaseapp.com',
+    projectId: 'sam-app-c17ad',
+    storageBucket: 'sam-app-c17ad.appspot.com',
+    messagingSenderId: '1010518656103',
+    appId: '1:1010518656103:web:0ebefb6b374c6d816000c2',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAd7gm_M2TXbTpOtfQ_4rqBZLiBm2LyC6c',
+    authDomain: 'sam-app-c17ad.firebaseapp.com',
+    projectId: 'sam-app-c17ad',
+    storageBucket: 'sam-app-c17ad.appspot.com',
+    messagingSenderId: '1010518656103',
+    appId: '1:1010518656103:web:0ebefb6b374c6d816000c2',
+  },
 };
 
 /*
